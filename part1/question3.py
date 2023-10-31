bef41ec23630dfba313bde719440bc36096da686
Leonardo Andres Combita Castilla <129130867+Leonardo-Andres-Combita-Castilla@users.noreply.github.com>
2023-10-31 16:29:52 -0500

    Update question3.py
    Se creo la clase Oven la cual representa al horno magico, el horno recibe los atributos de ingredientes, tempreatura y resultado.
    contiene el metodo add(self, item) el cual permite agregar los ingredientes.
    el metodo freeze(self) congela los ingredientes si la temperatura es menor a 0
    el metodo boil(self) hierve los ingredientes si la temperatura es mayor o igual a 100
    el metodo wait(self) se utiliza cuando no hay cambios en la tempreatura
    el metodo get_output(self) retorna el resultado de la combinacion de ingredientes y tempreatura en el horno
    por ultimo la funcion make_oven() nos permite crear un horno nuevo con nuevos atributos

--- part1/question3.py
+++ part1/question3.py
@@ -26,8 +26,38 @@
 # formulas and their outputs in the test file, `question3_test.py`.
 
 # This function should return an oven instance!
+class Oven:
+    def __init__(self):
+        self.ingredients = []
+        self.temperature = 0
+        self.output = None
+
+    def add(self, item):
+        self.ingredients.append(item)
+
+    def freeze(self):
+        self.temperature = min(0, self.temperature)
+
+    def boil(self):
+        self.temperature = max(100, self.temperature)
+
+    def wait(self):
+        pass  
+
+    def get_output(self):
+        if self.temperature < 0:
+            self.output = "snow"
+        elif self.temperature >= 100:
+            if "lead" in self.ingredients and "mercury" in self.ingredients:
+                self.output = "gold"
+            elif "water" in self.ingredients and "air" in self.ingredients:
+                self.output = "steam"
+            elif "cheese" in self.ingredients and "dough" in self.ingredients and "tomato" in self.ingredients:
+                self.output = "pizza"
+        return self.output
+
 def make_oven():
-  None
+  return Oven()
 
 def alchemy_combine(oven, ingredients, temperature):
   
