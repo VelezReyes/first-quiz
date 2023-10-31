1a1a3b16552df347052bf3c6e4cd57263b6881ae
Leonardo Andres Combita Castilla <129130867+Leonardo-Andres-Combita-Castilla@users.noreply.github.com>
2023-10-31 13:21:26 -0500

    Update question2.py
    Se creo la funcion swapper la cual recibe como argumento una tumpla, en este caso x, y = tupla es el equivalente a: x = tupla[0] 
    y = tupla[1], en el cual se reciben los argumentos y se les asigna el valor x ó y dependiendo de su psicion para posteriormente retornar como respuesta (y, x)

--- part1/question2.py
+++ part1/question2.py
@@ -15,7 +15,9 @@
 # it. As always, there is a test suite that checks the result. It is in 
 # `question2_test.py.`
 
-swapper = None
+def swapper(tupla):
+  x, y = tupla
+  return (y, x)
 
 def run_swapper(list_of_tuples):
   return list(map(swapper, list_of_tuples))
