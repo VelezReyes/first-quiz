0c6073f3b7531b2b6a3b3d98823ce0f9c5965901
Armosque <101122672+Armosque@users.noreply.github.com>
2023-11-03 14:56:19 -0500

    Desarrollando Parte 2

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,48 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+public class VendingMachineImpl implements VendingMachine {
+  private int quartersInserted;
+  private boolean machineReset;
+
+  public VendingMachineImpl() {
+    quartersInserted = 0;
+    machineReset = false;
+  }
+
+  @Override
+  public void insertQuarter() {
+    quartersInserted++;
+    machineReset = false;
+  }
+
+  @Override
+  public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+    if (machineReset) {
+      throw new NotEnoughMoneyException();
+    }
+
+    if (name.equals("ScottCola")) {
+      if (quartersInserted >= 3) {
+        machineReset = true;
+        quartersInserted -= 3;
+        return new ScottCola();
+      } else {
+        throw new NotEnoughMoneyException();
+      }
+    } else if (name.equals("KarenTea")) {
+      if (quartersInserted >= 4) {
+        machineReset = true;
+        quartersInserted -= 4;
+        return new KarenTea();
+      } else {
+        throw new NotEnoughMoneyException();
+      }
+    } else {
+      throw new UnknownDrinkException();
+    }
+  }
 
   public static VendingMachine getInstance() {
-    // Fix me!
-    return null;
+    return new VendingMachineImpl();
   }
 }
