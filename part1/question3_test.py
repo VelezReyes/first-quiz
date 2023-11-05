83d3e4d64ae0471a1b47ea321968b34ae3342e05
Andres Felipe <57474509+Andres1410@users.noreply.github.com>
2023-11-05 12:38:42 -0500

    Update question3_test.py
    espacios

--- part1/question3_test.py
+++ part1/question3_test.py
@@ -1,18 +1,18 @@
 def test_alchemy_combine():
 
-  assert alchemy_combine(
+assert alchemy_combine(
     make_oven(),
     ["lead", "mercury"],
     5000
   ) == "gold"
 
-  assert alchemy_combine(
-    make_oven(),
+assert alchemy_combine(
+   make_oven(),
     ["water", "air"],
     -100
   ) == "snow"
 
-  assert alchemy_combine(
+assert alchemy_combine(
     make_oven(),
     ["cheese", "dough", "tomato"],
     150
